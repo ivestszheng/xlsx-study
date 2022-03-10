7c2ee5af754419aa933a95f1beffa970e086c4d2
无声 <ivestszheng@qq.com>
2022-03-11 01:18:55 +0800

    chore: jest

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,15 @@
-// import { shallowMount } from '@vue/test-utils';
-// import HelloWorld from '../../src/components/HelloWorld.vue';
+import { shallowMount } from '@vue/test-utils';
+import HelloWorld from '../../src/components/HelloWorld.vue';
 
-// describe('HelloWorld.vue', () => {
-//   it('renders props.msg when passed', () => {
-//     const msg = 'new message';
-//     const wrapper = shallowMount(HelloWorld, {
-//       propsData: { msg }
-//     });
-//     expect(wrapper.text()).toMatch(msg);
-//   });
+describe('HelloWorld.vue', () => {
+  it('renders props.msg when passed', () => {
+    const msg = 'new message';
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg }
+    });
+    expect(wrapper.text()).toMatch(msg);
+  });
+});
+// test('adds 1 + 2 to equal 3', () => {
+//   expect(1 + 2).toBe(3);
 // });
