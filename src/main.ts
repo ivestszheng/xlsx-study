b64e8bca38455cdb77cd152e78a86ab9197b38f1
无声 <ivestszheng@qq.com>
2022-03-11 15:57:22 +0800

    build(deps): add composition-api dep

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import VueCompositionAPI from '@vue/composition-api';
 import App from './App.vue';
 // eslint-disable-next-line import/extensions
 import router from './router';
 
 Vue.config.productionTip = false;
+Vue.use(VueCompositionAPI);
 
 new Vue({
   router,
---
+++ src/views/CompositionApi.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ state.count }}</h1>
+    <button @click="addCount">加</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { reactive } from '@vue/composition-api'
+
+export default {
+  setup() {
+    const state = reactive({
+      count: 0
+    })
+    const addCount = () => {
+      state.count += 1
+    }
+    return { state, addCount }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+a {
+  color: #42b983;
+}
+
+label {
+  margin: 0 0.5em;
+  font-weight: bold;
+}
+
+code {
+  background-color: #eee;
+  padding: 2px 4px;
+  border-radius: 4px;
+  color: #304455;
+}
+</style>
