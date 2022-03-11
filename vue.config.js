a3f925f61e3a7d3ba11dbb1f55ef45ed2202b12c
无声 <ivestszheng@qq.com>
2022-03-11 14:20:31 +0800

    build(deps): update webpack and vue.config

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true
 })
--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 
-
 module.exports = {
     resolve: {
         // 支持缩写
@@ -9,9 +8,6 @@ module.exports = {
         // 别名
         alias: {
             '@': path.join(__dirname, 'src'), // "@表示src目录，即\src"
-            '@source': path.join(__dirname, 'src', 'source'), // 静态资源
-            '@com': path.join(__dirname, 'src', 'components'), // 组件目录
-            'react-dom': '@hot-loader/react-dom',
         }
     }
 }
