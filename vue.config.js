080f05e22f8d40242b5d324567714b0df0300509
无声 <ivestszheng@qq.com>
2022-03-22 15:32:36 +0800

    style(other): change To LF

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './'
-})
+  publicPath: './',
+});
--- webpack.common.js
+++ webpack.common.js
@@ -2,12 +2,12 @@
 const path = require('path');
 
 module.exports = {
-    resolve: {
-        // 支持缩写
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        // 别名
-        alias: {
-            '@': path.join(__dirname, 'src'), // "@表示src目录，即\src"
-        }
-    }
-}
+  resolve: {
+    // 支持缩写
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    // 别名
+    alias: {
+      '@': path.join(__dirname, 'src'), // "@表示src目录，即\src"
+    },
+  },
+};
