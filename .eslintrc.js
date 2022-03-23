1eefafd7d20a41aa44c2673826955f0952bd487b
无声 <ivestszheng@qq.com>
2022-03-23 16:14:09 +0800

    build(修改 alias 及 resolver 配置): build(other): update webpack config

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,7 +24,8 @@ module.exports = {
   settings: {
     'import/resolver': {
       webpack: {
-        config: 'webpack.config.js',
+        // 此处config对应webpack.config.js的路径，我这个路径是vue-cli3默认的路径
+        config: 'node_modules/@vue/cli-service/webpack.config.js',
       },
     },
   },
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,11 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: './',
+  chainWebpack: (config) => {
+    // 配置别名后需要重启项目
+    config.resolve.alias.set('@', path.resolve('src'));
+  },
 });
