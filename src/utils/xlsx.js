2fbefcf870c8aef69b3de645c42e1a6af39738d6
无声 <ivestszheng@qq.com>
2022-05-11 08:13:57 +0800

    feat: 增加 upload

---
+++ src/utils/xlsx.js
@@ -0,0 +1,8 @@
+import XLSX from 'xlsx';
+
+const analyseExcel = () => {
+  console.log(123);
+};
+
+// eslint-disable-next-line import/prefer-default-export
+export { analyseExcel };
--- src/views/Demo/index.vue
+++ src/views/Demo/index.vue
@@ -1,15 +1,75 @@
 <template>
   <div class="demo">
-    <el-button type="primary">导入按钮</el-button>
+    <el-upload
+      ref="upload"
+      class="upload-demo"
+      action="#"
+      multiple
+      :before-upload="beforeExcelUpload"
+      :on-preview="handlePreview"
+      :on-remove="handleRemove"
+      :file-list="fileList"
+      :auto-upload="true"
+      :http-request="handleHttpRequest"
+    >
+      <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+      <el-button style="margin-left: 10px" size="small" type="success" @click="analyseUpload">读取数据</el-button>
+      <div slot="tip" class="el-upload__tip">在根目录下有一个 Demo.excel</div>
+    </el-upload>
   </div>
 </template>
 
 <script>
+import { analyseExcel } from '@/utils/xlsx';
+
 export default {
   name: 'ImportExcel',
-  components: {},
   data() {
-    return {};
+    return {
+      fileList: [],
+    };
+  },
+  methods: {
+    analyseUpload() {
+      console.log('读取数据');
+      console.log(this.fileList);
+      analyseExcel();
+    },
+    // 文件列表移除文件时的钩子
+    handleRemove(file) {
+      const { uid } = file;
+
+      for (let i = 0; i < this.fileList.length; i += 1) {
+        const ele = this.fileList[i];
+
+        if (uid === ele.uid) {
+          this.fileList.splice(i, 1);
+          break;
+        }
+      }
+      console.log(file, this.fileList);
+    },
+    // 点击文件列表中已上传文件时的钩子
+    handlePreview(file) {
+      console.log(file);
+    },
+    handleHttpRequest(incomeFile) {
+      const { file } = incomeFile;
+      const { name } = file;
+
+      this.fileList.push({ name, file });
+    },
+    // 上传文件之前的钩子，参数为上传的文件，若返回 false 或者返回 Promise 且被 reject，则停止上传。
+    beforeExcelUpload(file) {
+      if (!file) {
+        return false;
+      }
+      if (!/\.(xls|xlsx)$/.test(file.name.toLowerCase())) {
+        this.$message.error('上传格式不正确，请上传 xls 或者 xlsx 格式');
+        return false;
+      }
+      return true;
+    },
   },
 };
 </script>
